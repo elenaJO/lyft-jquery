9438bbfa6c89cfaa4aa5940d3c1bb66cab375a92
elenaJO <ele.lucero1993@gmail.com>
2017-12-25 21:51:58 -0500

    añadiendo eventos botones atras

--- js/form.js
+++ js/form.js
@@ -2,29 +2,32 @@ $(document).ready(function() {
   $('#first-name').keyup(function() {
     if ($(this).val().match(/[1-9]/)) {
       alert('No ingresar numeros');
+      $(this).val('');
     } 
   });
 
   $('#last-name').keyup(function() {
     if ($(this).val().match(/[1-9]/)) {
       alert('No ingresar numeros');
+      $(this).val('');
     } 
   });
 
-  $('#submit').click(function() {
-    // $('#email').on('input', function() {
-    //   var PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    //   var res = PATTERNEMAIL.test($(this).val());
-    //   alert(res);
-    // });
+  $('#email').focusout(function() {
     var PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    var res = PATTERNEMAIL.test($('#email').val());
-    alert(res);
-    if ($('#check').prop('checked') && $('#first-name').val() && $('#last-name').val() && $('#email').val() && res) {
+    var res = PATTERNEMAIL.test($(this).val());
+    if (res === false) {
+      alert('Ingresar correo valido');
+      $(this).val('');
+    }
+  });
+  $('#submit').click(function(event) {
+    if ($('#check').prop('checked') && $('#first-name').val() && $('#last-name').val() && $('#email').val()) {
+      event.preventDefault();
       window.location.href = '../views/end.html';
-      alert('lo logramos');
     } else {
       alert('Marque todos los campos');
+      event.preventDefault();
     }
   });
 });
